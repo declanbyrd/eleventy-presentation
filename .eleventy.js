7365eae1c8cbb0f0a139fd3b1775ef0547f0003f
Declan Byrd <declan.byrd@gmail.com>
2020-10-22 11:50:11 +0100

    feat: Added slides
    - Slides document advantages of 11ty.
    - Added document title.
    - Syntax highlighting and inclusive language plugins.

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,13 @@
+const syntaxHighlighting = require('@11ty/eleventy-plugin-syntaxhighlight');
+const inclusiveLangPlugin = require('@11ty/eleventy-plugin-inclusive-language');
+
 module.exports = (eleventyConfig) => {
     eleventyConfig.addCollection('slides', (collection) =>
         collection.getFilteredByGlob('src/content/slides/*.md')
     );
-
+    eleventyConfig.addPlugin(syntaxHighlighting, { templateFormats: 'md' });
+    eleventyConfig.addPlugin(inclusiveLangPlugin);
+    
     eleventyConfig.addPassthroughCopy('src/js');
 
     return {
